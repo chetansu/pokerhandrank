23e683f7d8b56cc831ac24c48fe14d0e6868ba3d
Chetan Suri <chetan.suri@gmail.com>
2022-12-01 19:11:43 +0530

    added mocha test cases

---
+++ test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const pokerhand = require('./pokerhand');
+
+describe('Check Poker rank of various hands', function () {
+  const tests = [
+    {args: ["hearts-A", "hearts-K", "clubs-J", "diamonds-8", "hearts-J", "hearts-Q", "hearts-10"], expected: "ROYAL_FLUSH"},
+    {args: ["hearts-6", "hearts-3", "clubs-4", "diamonds-8", "hearts-4", "hearts-5", "hearts-2"], expected: "STRAIGHT_FLUSH"},
+    {args: ["hearts-9", "hearts-3", "clubs-4", "diamonds-8", "hearts-4", "hearts-5", "hearts-2"], expected: "FLUSH"},
+    {args: ["hearts-7", "hearts-3", "clubs-7", "diamonds-7", "spades-7", "hearts-5", "spades-4"], expected: "FOUR_OF_A_KIND"},
+    {args: ["hearts-4", "hearts-3", "clubs-4", "diamonds-4", "hearts-7", "clubs-5", "spades-5"], expected: "FULLHOUSE"},
+    {args: ["hearts-5", "hearts-3", "clubs-4", "diamonds-2", "hearts-7", "clubs-5", "spades-5"], expected: "THREE_OF_A_KIND"},
+    {args: ["hearts-4", "hearts-3", "clubs-3", "diamonds-4", "hearts-7", "clubs-9", "spades-5"], expected: "TWO_PAIR"},
+    {args: ["hearts-4", "hearts-A", "clubs-4", "diamonds-6", "hearts-7", "clubs-9", "spades-5"], expected: "ONE_PAIR"},
+    {args: ["hearts-4", "hearts-7", "clubs-9", "diamonds-K", "hearts-J", "clubs-2", "spades-5"], expected: "HIGH_CARD"}
+  ];
+
+  tests.forEach(({args, expected}) => {
+    it(`cards in hand are : ${args}`, function () {
+      const res = pokerhand.checkRank(args);
+      assert.strictEqual(res.wintype, expected);
+    });
+  });
+});
